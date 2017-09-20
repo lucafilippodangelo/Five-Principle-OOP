d8caba1dcfb136de5ea5babcac2f638fa8eed35f
lucafilippodangelo <info@lucadangelo.it>
2017-09-20 07:00:25 +0100

    first

---
+++ Five Principle OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Five_Principle_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
